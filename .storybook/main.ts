ddb441b6f1cf1ea2e8189b3f8466425809f3218f
Dasha21-lab <chernyshov387@gmail.com>
2025-12-03 15:17:54 +0300

    Project

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import path from 'node:path';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   "stories": [
@@ -10,11 +12,26 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
   ],
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
-  }
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../src'),
+          '@app': path.resolve(__dirname, '../src/app'),
+          '@entities': path.resolve(__dirname, '../src/entities'),
+          '@features': path.resolve(__dirname, '../src/features'),
+          '@widgets': path.resolve(__dirname, '../src/widgets'),
+          '@pages': path.resolve(__dirname, '../src/pages'),
+          '@shared': path.resolve(__dirname, '../src/shared')
+        },
+      },
+    })
+  },
 };
+
 export default config;
